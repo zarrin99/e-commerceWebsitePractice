d6064177a31f5f1bc551bcc3b54863dc97dccd43
zarrin99 <zarrintasnim99@gmail.com>
2025-07-27 11:37:16 +0600

    fetch header and footer
    separated header and footer into their own html files and used fetch method. this way they load for every page in the website when called and placed placeholders accordingly

---
+++ loadComponents.js
@@ -0,0 +1,13 @@
+// Load Header
+fetch('header.html')
+  .then(response => response.text())
+  .then(data => {
+    document.getElementById('header-placeholder').innerHTML = data;
+  });
+
+// Load Footer
+fetch('footer.html')
+  .then(response => response.text())
+  .then(data => {
+    document.getElementById('footer-placeholder').innerHTML = data;
+  });
